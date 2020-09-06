e2aae7796861a0fa6172d95a868a964bc23af206
Virgil Muoghara <v.muoghara@gmail.com>
2020-09-06 17:42:21 +0300

    Added all the files

---
+++ kitten.js
@@ -0,0 +1,20 @@
+console.log('Kittens is running');
+
+let filename = [
+    "cat0.jpg",
+    "cat1.jpg",
+    "cat2.jpg",
+    "cat3.jpg",
+    "cat4.jpg",
+    "cat5.jpg",
+]; 
+
+let imgs = document.getElementsByTagName('img');
+
+for(imgElt of imgs){
+    let ran = Math.floor(Math.random() * filename.length);
+    let file = 'kittens/' + filename[ran];
+    let url = chrome.extension.getURL(file);
+    imgElt.src = url;
+    console.log("image scr = " + url);
+}
